d970a6d5bdd203f210e8467c7c37ea6f9ae10afa
Aayush Kapoor <xeoneux@icloud.com>
2018-12-18 01:51:56 +0530

    Create vertex input for triangle

--- 1_hello_window.cpp
+++ 1_hello_window.cpp
@@ -1,7 +1,6 @@
+#include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
-#include <iostream>
 
 int main() {
   glfwInit();
---
+++ 2_hello_triangle.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+  auto *window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+
+  if (window == nullptr) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window,
+                                 [](auto *window, auto width, auto height) {
+                                   glViewport(0, 0, width, height);
+                                 });
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  float vertices[] = {
+      -0.5f, -0.5f, 0.0f, //
+      0.5f,  -0.5f, 0.0f, //
+      0.0f,  0.5f,  0.0f  //
+  };
+
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  while (!glfwWindowShouldClose(window)) {
+
+    if (GLFW_PRESS == glfwGetKey(window, GLFW_KEY_ESCAPE))
+      glfwSetWindowShouldClose(window, true);
+
+    glClearColor(0.2, 0.3, 0.4, 0.5);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
+}
